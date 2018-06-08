b0b71cc21a8405c908155c7ae5efd676b44802ad
dnfuller <dnfuller92@gmail.com>
2018-06-07 19:13:29 -0600

    Final upload

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stack Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portabella Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwestern Burger', false);
